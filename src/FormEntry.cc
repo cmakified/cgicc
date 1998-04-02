a0d368e1de25b647a1c102fa6c372899940f79ed
sbooth <sbooth>
1998-04-02 11:10:15 +0000

    *** empty log message ***

--- src/FormEntry.cc
+++ src/FormEntry.cc
@@ -1,4 +1,4 @@
-/* $Id: FormEntry.cc,v 1.2 1998/04/01 20:52:21 sbooth Exp $ */
+/* $Id: FormEntry.cc,v 1.3 1998/04/02 11:10:15 sbooth Exp $ */
 
 #include "FormEntry.hh"
 
@@ -39,4 +39,134 @@ FormEntry::~FormEntry()
 void
 FormEntry::render(ostream& out) const {
   out << getName() << '=' << getValue();
+}
+
+void 
+FormEntry::getValue(int maxChars,
+		    char* &value) const throw(Exception) 
+{
+  value = new char[ strlen(getValue()) +1 ];
+  if( ! value )
+    throw Exception("new failed", ERRINFO);
+  int result = makeString(value, maxChars, true);
+  if( ! result ) {
+    delete [] value;
+    value = NULL;
+  }
+}
+
+void 
+FormEntry::getValueStripped(char* &value) const throw(Exception) {
+  getValueStripped(INT_MAX, value);
+}
+
+void 
+FormEntry::getValueStripped(int maxChars,
+			    char* &value) const throw(Exception) 
+{
+  value = new char[ strlen(getValue()) +1 ];
+  if( ! value )
+    throw Exception("new failed", ERRINFO);
+  int result = makeString(value, maxChars, false);
+  if( ! result) {
+    delete [] value;
+    value = NULL;
+  }
+}
+
+int
+FormEntry::getIntegerValue(int min, int max) const {
+  int value = atoi(getValue());
+  if(value > max)
+    value = max;
+  else if(value < min)
+    value = min;
+  
+  return value;
+}
+
+double
+FormEntry::getDoubleValue(double min, double max) const {
+  double value = atof(getValue());
+  if(value > max)
+    value = max;
+  else if(value < min)
+    value = min;
+  
+  return value;
+}
+
+int 
+FormEntry::makeString(char*	&result, 
+		      int 	maxLen, 
+		      int 	allowNewlines) const
+{
+  char *dest, *s;
+  int truncated = 0;
+  int len 	= 0;
+  int avail 	= maxLen - 1;
+  int crCount 	= 0;
+  int lfCount 	= 0;	
+
+  dest 	= result;
+  s 	= (char*) getValue();	
+
+  while(true) {
+    char c = *s;
+
+    if( len == avail ) {
+      truncated = true;
+      break;
+    }	 
+		
+    // Fix the CR/LF, LF, CR nightmare: watch for
+    // consecutive bursts of CRs and LFs in whatever
+    // pattern, then actually output the larger number 
+    // of LFs. Consistently sane, yet it still allows
+    // consecutive blank lines when the user
+    // actually intends them.
+    if( ( c == 13 ) || ( c == 10 ) ) 
+      if( c == 13 ) 
+	crCount++;
+      else 
+	lfCount++;
+    else {
+      if( crCount || lfCount ) {
+	int lfsAdd = crCount;
+	if( lfCount > crCount ) 
+	  lfsAdd = lfCount;
+	
+	// Stomp all newlines if desired
+	if ( ! allowNewlines ) 
+	  lfsAdd = 0;
+	
+	while( lfsAdd ) {
+	  if( len == avail ) {
+	    truncated = true;
+	    break;
+	  }
+	  *dest++ = 10;
+	  lfsAdd--;
+	  len++;		
+	}
+	crCount = 0;
+	lfCount = 0;
+      }
+      if( c == '\0' ) {
+	// The end of the source string
+	break;				
+      }
+      *dest++ = c;
+      len++;
+    }
+    s++;	
+  }
+  *dest = '\0';
+  
+  if( truncated ) 
+    return -1;
+  else if( ! len) 
+    return 0;
+  else 
+    return 1;
 }
