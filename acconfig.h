4426b695ad1bcdfa5683c18a43a710a20db41d44
sbooth <sbooth>
1999-04-26 22:41:54 +0000

    Converted to use STL

--- acconfig.h
+++ acconfig.h
@@ -1,20 +1,20 @@
 /*
- *  $Id: acconfig.h,v 1.1 1999/01/06 22:07:00 sbooth Exp $
+ *  $Id: acconfig.h,v 1.2 1999/04/26 22:41:54 sbooth Exp $
  *
  *  Copyright (C) 1996, 1997, 1998, 1999 Stephen F. Booth
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Library General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Library General Public
- *  License along with this library; if not, write to the Free
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
@@ -23,3 +23,9 @@
 
 /* Version of package. */
 #undef VERSION
+
+/* System Cgicc was configured for */
+#define HOST ""
+
+/* Whether to use namespaces */
+#define USE_NAMESPACES 1
--- acinclude.m4
+++ acinclude.m4
@@ -1,4 +1,6 @@
-dnl $Id: acinclude.m4,v 1.2 1999/02/02 18:25:43 sbooth Exp $
+dnl $Id: acinclude.m4,v 1.3 1999/04/26 22:41:55 sbooth Exp $
+
+dnl @TOP@
 
 dnl CGICC_CHECK_LINK_STDCPP
 AC_DEFUN(CGICC_CHECK_LINK_STDCPP, [
@@ -55,5 +57,44 @@ AC_DEFUN(CGICC_CHECK_CPP_TEMPLATES, [
 		AC_MSG_ERROR([template support required])
 	fi
 ])
+
+
+dnl CGICC_CHECK_CPP_STL
+AC_DEFUN(CGICC_CHECK_CPP_STL, [
+	AC_REQUIRE([AC_PROG_CXX])
+	AC_CACHE_CHECK(	
+		[whether the C++ compiler ($CXX) supports STL],
+		[cgicc_cv_cpp_stl],
+		[	AC_LANG_SAVE
+			AC_LANG_CPLUSPLUS
+			AC_TRY_CPP([#include <vector> 
+std::vector<int> gInts;],
+			cgicc_cv_cpp_stl=yes,
+			cgicc_cv_cpp_stl=no)
+			AC_LANG_RESTORE
+		])
+	if (test "$cgicc_cv_cpp_stl" = no); then 
+		AC_MSG_ERROR([standard template library support required])
+	fi
+])
+
+dnl CGICC_CHECK_CPP_NAMESPACES
+AC_DEFUN(CGICC_CHECK_CPP_NAMESPACES, [
+	AC_REQUIRE([AC_PROG_CXX])
+	AC_CACHE_CHECK(	
+		[whether the C++ compiler ($CXX) supports namespaces],
+		[cgicc_cv_cpp_namespaces],
+		[	AC_LANG_SAVE
+			AC_LANG_CPLUSPLUS
+			AC_TRY_COMPILE([namespace cgicc {void foo() {} }],
+			cgicc::foo();,
+			cgicc_cv_cpp_namespaces=yes,
+			cgicc_cv_cpp_namespaces=no)
+			AC_LANG_RESTORE
+		])
+])
+
+
+
 
 
