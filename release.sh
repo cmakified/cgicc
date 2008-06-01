70b46cd749e151b13e9cafda95edcacdb6ec91aa
sebdiaz <sebdiaz>
2008-06-01 18:40:27 +0000

    BUG 23341 BUG 23316

--- release.sh
+++ release.sh
@@ -5,13 +5,21 @@ rm -rf cgicc-$1/*
 rm -rf cgicc-$1
 cd -
 mkdir /tmp/cgicc-$1
+./autogen.sh
 cp -Rf * /tmp/cgicc-$1  
 mv /tmp/cgicc-$1 .
 cd cgicc-$1
 echo "s/NOTVERSIONNEDPACKAGE/"$1"/" >tempGen.sed
+echo "Writing"
 sed -f tempGen.sed configure.ac>temp2
 rm configure.ac
+sed -f tempGen.sed configure>temp21
+rm configure
+mv temp21 configure
+chmod +x configure
 mv temp2 configure.ac
+sed -f tempGen.sed Makefile.in>temp22
+rm temp2 temp21 temp22
 sed -f tempGen.sed doxygen.conf>temp3
 rm doxygen.conf
 mv temp3 doxygen.conf
